fa70f8691db1afa1e65d9fcc1b96e329c2cbfe95
777777-ops <777777-ops@1131956253@qq.com>
2025-11-29 21:31:24 +0800

    双控开关

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,26 @@
+#include "Vtop.h"
+#include "verilated.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <nvboard.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+void nvboard_bind_all_pins(Vtop* top);
+
+int main(int argc, char** argv) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop* top = new Vtop{contextp};
+
+    //接入nvboard
+    nvboard_bind_all_pins(top);
+    nvboard_init();
+
+    while (true) {
+	nvboard_update();
+	top->eval();
+    }
+    delete top;
+    delete contextp;
+    return 0;
 }
